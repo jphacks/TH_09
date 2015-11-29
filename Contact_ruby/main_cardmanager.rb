80ac8f005c847c163ab92f1e0fc9a4aeb87a33ee
t-kanai <t-kanai@t-kanai-no-MacBook-Air.local>
2015-11-29 12:42:27 +0900

    third commit

--- Contact_ruby/main_cardmanager.rb
+++ Contact_ruby/main_cardmanager.rb
@@ -1,7 +1,25 @@
 #! /bin/ruby
 
-require 'kintone'
+# main
+puts "操作を選んでください"
+handle = -1
 
+# Loop
+while true
+  puts "0:終了"
+  puts "1:登録カードの一覧"
+  handle = gets.to_i
 
-require './test.rb'
-puts 'aa'
+#Exit
+  if handle == 0 then
+    exit
+#Error Output
+  elsif handle != 1
+    puts "数値が正しくありません"
+#Get All Card Name
+  elsif handle == 1
+    require './CardNameGet.rb'
+  end
+
+  puts " "
+end
